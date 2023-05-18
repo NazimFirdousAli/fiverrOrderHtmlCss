476a466f78c1c016646e3d2f465b86d0fe324506
nazim.mandviwala98 <nazim.mandviwala98@gmail.com>
2023-05-18 18:12:35 +0500

    working

--- app.js
+++ app.js
@@ -1,71 +1,121 @@
-window.chartColors = {
-    red: 'rgb(255, 99, 132)',
-    orange: 'rgb(255, 159, 64)',
-    yellow: 'rgb(255, 205, 86)',
-    green: 'rgb(75, 192, 192)',
-    blue: 'rgb(54, 162, 235)',
-    purple: 'rgb(153, 102, 255)',
-    grey: 'rgb(231,233,237)'
-  };
-  alert("00000000")
-  var randomScalingFactor = function() {
-    return (Math.random() > 0.5 ? 1.0 : 1.0) * Math.round(Math.random() * 100);
-  };
-  
-  var line1 = [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), ];
-  
-  var line2 = [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), ];
-  
-  var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var config = {
-    type: 'line',
-    data: {
-      labels: MONTHS,
-      datasets: [{
-        label: "My First dataset",
-        backgroundColor: window.chartColors.red,
-        borderColor: window.chartColors.red,
-        data: line1,
-        fill: false,
-      }, {
-        label: "My Second dataset",
-        fill: false,
-        backgroundColor: window.chartColors.blue,
-        borderColor: window.chartColors.blue,
-        data: line2,
-      }]
+/**
+ * Create a chart
+ */
+var chart = AmCharts.makeChart("chartdiv", {
+  type: "serial",
+  theme: "light",
+  addClassNames: true,
+  legend: {
+    horizontalGap: 10,
+    maxColumns: 1,
+    position: "right",
+    useGraphSettings: true,
+    markerSize: 10,
+    listeners: [
+      {
+        event: "rollOverItem",
+        method: function (event) {
+          setOpacity(event.chart.graphs[event.dataItem.index], 1);
+        },
+      },
+      {
+        event: "rollOutItem",
+        method: function (event) {
+          setOpacity(event.chart.graphs[event.dataItem.index], 0.5);
+        },
+      },
+    ],
+  },
+  dataProvider: [
+    {
+      year: 2003,
+      Dollar: 2.5,
     },
-    options: {
-      responsive: true,
-      title:{
-        display:true,
-        text:'Chart.js Line Chart'
-      },
-      tooltips: {
-        mode: 'index',
-        intersect: false,
-      },
-     hover: {
-        mode: 'nearest',
-        intersect: true
-      },
-      scales: {
-        xAxes: [{
-          display: true,
-          scaleLabel: {
-            display: true,
-            labelString: 'Month'
-          }
-        }],
-        yAxes: [{
-          display: true,
-          scaleLabel: {
-            display: true,
-          },
-        }]
-      }
+    {
+      year: 2004,
+      Dollar: 2.6,
+    },
+    {
+      year: 2005,
+      Dollar: 2.8,
+    },
+    {
+      year: 2005,
+      Dollar: 2.8,
+    },
+    {
+      year: 2005,
+      Dollar: 2.8,
+    },
+    {
+      year: 2005,
+      Dollar: 2.8,
+    },
+    {
+      year: 2005,
+      Dollar: 2.8,
+    },
+  ],
+  valueAxes: [
+    {
+      stackType: "regular",
+      axisAlpha: 0.3,
+      gridAlpha: 0.1,
+      color: "white",
+    },
+  ],
+  graphs: [
+    {
+      id: "g1",
+      bullet: "round",
+      bulletAlpha: 0.3,
+      lineAlpha: 0.3,
+      lineThickness: 3,
+      title: "Dollar",
+      color: "#ffffff",
+      valueField: "Dollar",
+      labelText: "[[value]]",
+      lineColor: "#ffffff",
+    },
+  ],
+  categoryField: "year",
+  categoryAxis: {
+    gridAlpha: 0.1,
+    position: "left",
+    color: "white",
+  },
+});
+
+/**
+ * Define functions that set opacity of the line graphs
+ */
+chart.timeout;
+
+function setOpacity(graph, opacity) {
+  console.log(graph.id);
+  var className = "amcharts-graph-" + graph.id;
+  var items = document.getElementsByClassName(className);
+  if (undefined === items) return;
+  for (var x in items) {
+    if ("object" !== typeof items[x]) continue;
+    var path = items[x].getElementsByTagName("path")[0];
+    if (undefined !== path) {
+      // set line opacity
+      path.style.strokeOpacity = opacity;
     }
-  };
-  
-  var ctx = document.getElementById("canvas").getContext("2d");
-  var myLine = new Chart(ctx, config);
+
+    // set bullet opacity
+    var bullets = items[x].getElementsByClassName("amcharts-graph-bullet");
+    for (var y in bullets) {
+      if ("object" !== typeof bullets[y]) continue;
+      bullets[y].style.fillOpacity = opacity;
+    }
+
+    // set label opacity
+    var labels = items[x].getElementsByClassName("amcharts-graph-label");
+    for (var y in labels) {
+      if ("object" !== typeof labels[y]) continue;
+      labels[y].style.opacity = opacity == 1 ? 1 : 0;
+    }
+  }
+}
